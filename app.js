d964c54f52173f13cf15e36a7aab39b3405b99f7
noman318 <shaikhmohdnoman612@gmail.com>
2022-12-09 21:38:20 +0530

    User schema and screens updated

--- app.js
+++ app.js
@@ -14,6 +14,53 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("regis");
+});
+app.post("/postregis", (req, res) => {
+  uploadSingle(req, res, (err) => {
+    if (err) {
+      res.render("regis", { error: err.message });
+    } else {
+      let { email, uname, password } = req.body;
+      const hash = bcrypt.hashSync(password, saltRounds);
+      userModel
+        .create({
+          email: email,
+          username: uname,
+          password: hash,
+          image: req.file.filename,
+          status: 0,
+        })
+        .then((data) => {
+          let mailOptions = {
+            from: "markjackman31820@gmail.com",
+            to: email,
+            subject: "Activation Account",
+            template: "mail",
+            context: {
+              username: uname,
+              id: data._id,
+            },
+          };
+          transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect("/login");
+            }
+          });
+        })
+        .catch((err) => {
+          res.render("regis", { error: "User Already Registered" });
+        });
+    }
+  });
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Number,
+    required: true,
+  },
+});
+module.exports = mongoose.model("user", userSchema);
